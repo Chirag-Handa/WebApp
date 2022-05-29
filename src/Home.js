e864ee5cdd245bee838f055a07225181b486ee36
Chirag-Handa <chiraghanda2270@yahoo.com>
2022-05-30 00:14:39 +0530

    Added BG

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';
+import './Background.css';
 
 export class Home extends Component{
     render(){
         return(
             <div>
-                <h3>This is Home page</h3>
+                <h3 class="whiteTextOverride">This is Student Home Page</h3>
+                <div className='BGImage'></div>
             </div>
         )
     }
